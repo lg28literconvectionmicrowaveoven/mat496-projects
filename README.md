6414e7e04204faeb9e1900e76888ea76d733a737
Sabarigirish Manikandan <sabari.girish2057@gmail.com>
2025-09-05 13:05:39 +0530

    complete (i think? i don't have enough quota for embedding)

---
+++ README.md
@@ -0,0 +1,1 @@
+My (Sabarigirish Manikandan)'s projects made as practice for the MAT496 course (Introduction to LLMs) offered by Shiv Nadar University
--- lotr_rag/main.py
+++ lotr_rag/main.py
@@ -1,13 +1,9 @@
-# TODO: set up and use RedisVL as vector store
 from dotenv import load_dotenv
-from IPython.display import Markdown
 from langchain.chat_models import init_chat_model
+from langchain_community.document_loaders import TextLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_redis import RedisVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-with open("lotr.txt", "r") as file:
-    text = file.read()
 
 load_dotenv()
 
@@ -16,7 +12,8 @@ vector_store = RedisVectorStore(embeddings)
 splits = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=200,
-)
+).split_documents(TextLoader("./lotr_rag/lotr.txt").load())
+vector_store.add_documents(splits)
 search_results = vector_store.similarity_search_with_score(
     "Who does Frodo trust with the ring?",
     k = 10
@@ -24,7 +21,7 @@ search_results = vector_store.similarity_search_with_score(
 doc_content = "\n".join(doc[0].page_content for doc in search_results)
 
 prompt_template = """
-You are an assistant that reads literary works and provides accurate and irrefutable facts about them or in them as stated. Use the following pieces of text from the literary work under scrutiny as context and answer the question to the best of your ability. If you don't know the answer, just say you don't know. Do not try to make up an answer.
+You are an assistant that reads literary works and provides accurate and irrefutable facts about them or in them based solely on information stated. Use the following pieces of text from the literary work under scrutiny as context and answer the question to the best of your ability. If you don't know the answer, just say you don't know. Do not try to make up an answer.
 Question: {question}
 Context: {context}
 """
@@ -37,4 +34,4 @@ response = model.invoke(
     )
 )
 
-print(Markdown(response.content))
+print(response.content)
